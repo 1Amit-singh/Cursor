5b6112ccf26a45ef767a657ec3f65af3f2edf59a
Amit <iamitsinghthakur123@gmail.com>
2023-04-17 19:47:09 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mainBg: "#15142C",
+        belowNav1: "#AD1025",
+        belowNav2: "#0800c8",
+        section2Bg: "#DEDEDE"
+      }
+    },
+  },
+  plugins: [],
+}
+
