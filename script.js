ccb32cfc1a91f971fcca3fbbdbfa084c17c3fad8
Amit Singh Thakur <75479727+1Amit-singh@users.noreply.github.com>
2023-04-19 23:12:46 +0530

    updated html and js files

---
+++ script.js
@@ -0,0 +1,43 @@
+// for player 1 
+document.querySelector(".player_1").addEventListener("mouseover", function() {
+    document.querySelector(".player_a").classList.add("showplayer");
+});
+
+document.querySelector(".player_1").addEventListener("mouseout", function() {
+    document.querySelector(".player_a").classList.remove("showplayer");
+});
+
+
+
+// for player 2
+document.querySelector(".player_2").addEventListener("mouseover", function() {
+    document.querySelector(".player_b").classList.add("showplayer");
+});
+
+document.querySelector(".player_2").addEventListener("mouseout", function() {
+    document.querySelector(".player_b").classList.remove("showplayer");
+});
+
+
+
+// for player 3 
+document.querySelector(".player_3").addEventListener("mouseover", function() {
+    document.querySelector(".player_c").classList.add("showplayer");
+});
+
+document.querySelector(".player_3").addEventListener("mouseout", function() {
+    document.querySelector(".player_c").classList.remove("showplayer");
+});
+
+
+
+// for player 4 
+document.querySelector(".player_4").addEventListener("mouseover", function() {
+    document.querySelector(".player_d").classList.add("showplayer");
+});
+
+document.querySelector(".player_4").addEventListener("mouseout", function() {
+    document.querySelector(".player_d").classList.remove("showplayer");
+});
+
+
