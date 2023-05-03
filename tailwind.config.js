66544c33f4a45b85f953696a3761d48133e3d3d3
Amit Singh Thakur <75479727+1Amit-singh@users.noreply.github.com>
2023-05-03 09:29:54 -0700

    Added almost all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {
       colors: {
         mainBg: "#15142C",
         belowNav1: "#AD1025",
-        belowNav2: "#0800c8",
+        belowNav2: "rgba(8, 0, 200, 0.56)",
         section2Bg: "#DEDEDE"
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
